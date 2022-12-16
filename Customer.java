2067913dda623e83196411eeb1e2a635efe78277
Amirul Mu'minin <116171779+Amirul109@users.noreply.github.com>
2022-12-16 11:10:24 +0700

    Add files via upload

---
+++ Customer.java
@@ -0,0 +1,97 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Praktikum8;
+
+/**
+ *
+ * @author Amir
+ */
+ //Import class yang dibutuhkan
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.Scanner;
+public class Customer {
+    public static void main(String args[])
+    {
+        //Inisialisasi objek untuk masing-masing kelas
+         Scanner input = new Scanner(System.in);
+         DecimalFormat kursIndonesia = (DecimalFormat) DecimalFormat.getCurrencyInstance();
+         DecimalFormatSymbols formatRp = new DecimalFormatSymbols();
+         //Deklarasi variabel
+         int jum=0, i=0;
+         double total_bayar=0;
+         //Deklarasi array
+         int [] kode = new int[5];
+         int [] qty = new int[5];
+         int [] harga = new int[5];
+         double [] sub_total = new double[5];
+         double [] diskon= new double [5];
+         String [] barang = new String[5];
+         System.out.println("-------------------------------------");
+         System.out.println("PROGRAM JAVA TENTANG ORDER BARANG");
+         System.out.println("-------------------------------------");
+         
+         System.out.print("Masukan Jumlah Beli : ");
+         jum=input.nextInt();
+         System.out.println(" ");
+         //Memasukan elemen didalam array
+         for (i=0; i<jum;i++){
+              System.out.print("Masukan Kode Barang Ke-"+(i+1)+" : ");
+              kode[i]=input.nextInt();
+              System.out.print("Masukan qty Ke-"+(i+1)+" : ");
+              qty[i]=input.nextInt();
+              //Menentukan barang berdasarkan kode yang dimmasukan
+              switch (kode[i]){
+                  case 1 : 
+                            barang[i]="Bolpoint  ";
+                            harga[i]=3000;
+                            diskon[i]=0.1;
+                            break;
+                  case 2 : 
+                            barang[i]="kertas Hvs  ";
+                            harga[i]=5000;
+                            diskon[i]=0.05;
+                            break;
+                  case 3 : 
+                            barang[i]="Kertas folio";
+                            harga[i]=4000;
+                            diskon[i]=0;
+                            break;
+                  case 4 : 
+                            barang[i]="penggaris      ";
+                            harga[i]=6000;
+                            diskon[i]=0.2;
+                            break;
+                  case 5 : 
+                            barang[i]="penghapus    ";
+                            harga[i]=1500;
+                            diskon[i]=0.1;
+                            break;
+                  default : 
+                            System.out.println("Kode Barang Tidak Tersedia");
+              }
+         }
+          //Pengaturan format number
+          formatRp.setCurrencySymbol("Rp. ");
+          formatRp.setMonetaryDecimalSeparator(',');
+          formatRp.setGroupingSeparator('.');
+          kursIndonesia.setDecimalFormatSymbols(formatRp);
+          
+          System.out.println(" ");
+          System.out.println("-------------------------------------------------------------------");
+          System.out.println("|No | Nama Barang      |Harga    |QTY     |Diskon      | Total");  
+          System.out.println("-------------------------------------------------------------------");
+          //Menampilkan seluruh elemen di dalam array
+          for (i=0; i<jum;i++){ 
+             sub_total[i]=((qty[i]*harga[i])-(qty[i]*harga[i]*diskon[i]));
+             total_bayar+=sub_total[i];
+              System.out.println(i+1+"    "+barang[i]+"    "+kursIndonesia.format(harga[i])+"    "+qty[i]+"      "+(int)(diskon[i]*100)+"%"+"     "+kursIndonesia.format(sub_total[i])); 
+          }
+          System.out.println(" ");
+          //Menampilkan total bayar
+          System.out.println("Total Bayar : "+kursIndonesia.format(total_bayar));
+    }
+}
